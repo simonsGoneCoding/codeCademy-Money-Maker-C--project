8ad3d7542d2b1253931a15f9cebb5d76202d95f6
Szymon Warszawa <szymonwarszawa@gmail.com>
2021-03-02 21:12:19 +0000

    Complete all bulletpoints.

--- Money-Maker/Money-Maker/Program.cs
+++ Money-Maker/Money-Maker/Program.cs
@@ -6,7 +6,28 @@ namespace Money_Maker
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Capture Input 
+            Console.WriteLine("Welcome to Money Maker!");
+            Console.WriteLine("How much would you like to convert?");
+            string moneyStr = Console.ReadLine();
+            double moneyDouble = Convert.ToDouble(moneyStr);
+
+            Console.WriteLine($"{moneyStr} cents is equal to...");
+
+            // Convert to coins
+            int goldValue = 10;
+            int silverValue = 5;
+
+            double goldCoins = Math.Floor(moneyDouble / goldValue);
+            double reminder = moneyDouble % goldValue;
+
+            double silverCoins = Math.Floor(reminder / silverValue);
+            reminder = reminder % silverValue;
+
+            // Capture output
+            Console.WriteLine($"Gold coins: {goldCoins}");
+            Console.WriteLine($"Silver coins: {silverCoins}");
+            Console.WriteLine($"Bronze coins: {reminder}");
         }
     }
 }
